605b7b5a4c24229d26208233f9ac3d1c3b9836f6
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-04-30 15:07:33 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/Hique2605/Course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Hique2605.Course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Hique2605.Course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
