e3d9dc18fd132d58aa9b502d91ffc20ad02cea94
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-02 10:06:15 -0300

    Service layer, component registration

--- src/main/java/com/Hique2605/Course/repositories/UserRepository.java
+++ src/main/java/com/Hique2605/Course/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Hique2605.Course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
+	
 }
 
---
+++ src/main/java/com/Hique2605/Course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.Hique2605.Course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Hique2605.Course.entities.User;
+import com.Hique2605.Course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(long id ) {
+	Optional<User>	obj = repository.findById(id);
+	return obj.get();
+	}
+	
+}
