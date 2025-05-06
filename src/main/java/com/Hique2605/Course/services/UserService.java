84e8f14843fe1e188f641dbdf33ea34f7ea3cc68
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-06 11:50:54 -0300

    User update

--- src/main/java/com/Hique2605/Course/services/UserService.java
+++ src/main/java/com/Hique2605/Course/services/UserService.java
@@ -3,6 +3,7 @@ package com.Hique2605.Course.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,13 +25,26 @@ public class UserService {
 	Optional<User>	obj = repository.findById(id);
 	return obj.get();
 	}
-	
+	//cria user 
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+	//deleta user 
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	//update user 
+		public User update(Long id, User obj) {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		//metodo que atualiza, nao deixa atualizar senha e id 
+		private void updateData(User entity, User obj) {
+			entity.setName(obj.getName());
+			entity.setEmail(obj.getEmail());
+			entity.setPhone(obj.getPhone());
+		}
 	
 }
